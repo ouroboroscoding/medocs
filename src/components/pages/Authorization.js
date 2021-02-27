2871d1cc6fa2e7dacdc6c00c5fd08543fca9b429
Chris Nasr <ouroboroscode@gmail.com>
2021-02-27 10:02:54 -0500

    - Removed unnecessary import

--- src/components/pages/Authorization.js
+++ src/components/pages/Authorization.js
@@ -13,7 +13,6 @@ import React from 'react';
 
 // Material UI
 import Box from '@material-ui/core/Box';
-import Link from '@material-ui/core/Link';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 
